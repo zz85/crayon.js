b99bbeaf3ddc2387343190ec4636fb3b927b822c
Joshua Koo <zz85nus@gmail.com>
2014-04-08 07:12:44 +0800

    Revert "Trying out John Resig's Inheritance Style"
    This reverts commit eed0876d746736dab903ffa337f035014cdf2d54.

--- ShaderNodes.js
+++ ShaderNodes.js
@@ -18,74 +18,6 @@ window.CRAYON = {
 };
 
 
-/* Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
-// Inspired by base2 and Prototype
-(function(){
-  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
- 
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
- 
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop) {
-    var _super = this.prototype;
-   
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-   
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this._super;
-           
-            // Add a new ._super() method that is the same method
-            // but on the super-class
-            this._super = _super[name];
-           
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);        
-            this._super = tmp;
-           
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
-   
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the init method
-      if ( !initializing && this.init )
-        this.init.apply(this, arguments);
-    }
-   
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-   
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
- 
-    // And make this class extendable
-    Class.extend = arguments.callee;
-   
-    return Class;
-  };
-})();
-
-
-
-
 var width = innerWidth;
 var height = innerHeight;
 
@@ -93,7 +25,7 @@ var height = innerHeight;
 
 // A Shader Node has a render target and a render/pass/run/update function
 
-CRAYON.ShaderNode = Class.extend( {
+CRAYON.extends('ShaderNode', Object, {
 
 	init: function() {
 
@@ -125,15 +57,15 @@ CRAYON.ShaderNode = Class.extend( {
 		// TODO: Keep a list of connected Nodes
 	}
 
-} );
+});
 
 
 // Post Process Node uses a shader material
-CRAYON.PostProcessNode = CRAYON.ShaderNode.extend( {
+CRAYON.extends( 'PostProcessNode', CRAYON.ShaderNode, {
 
 	init: function (renderer, material) {
 
-		this._super();
+		CRAYON.ShaderNode.call( this )
 
 		this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
 		this.quad = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), null );
@@ -151,6 +83,7 @@ CRAYON.PostProcessNode = CRAYON.ShaderNode.extend( {
 		this.renderer.render( this.scene, this.camera, this.renderTarget );
 	}
 
+
 } );
 
 CRAYON.extends( 'InputConnector', Object, {
@@ -160,7 +93,7 @@ CRAYON.extends( 'InputConnector', Object, {
 		this.requirements = [];
 		this.connectedFrom = {};
 		this.node = node;
-
+		
 	},
 
 	requires: function() {
@@ -238,11 +171,11 @@ CRAYON.extends( 'InputConnector', Object, {
  *	Endpoint of graphs should lead to an executor node
  */
 
-CRAYON.ExecutorNode = CRAYON.ShaderNode.extend( {
+CRAYON.extends( 'ExecutorNode', CRAYON.ShaderNode, {
 
 	init: function ( renderer ) {
 
-		this._super( renderer );
+		CRAYON.ShaderNode.call( this, renderer );
 		this.inputs.requires('texture', 'gradientX', 'gradientY', 'edge');
 
 	},
@@ -275,7 +208,7 @@ CRAYON.ExecutorNode = CRAYON.ShaderNode.extend( {
 } );
 
 
-CRAYON.RenderToScreenNode = CRAYON.PostProcessNode.extend( {
+CRAYON.extends( 'RenderToScreenNode', CRAYON.PostProcessNode, {
 
 	init: function ( renderer ) {
 
@@ -296,7 +229,7 @@ CRAYON.RenderToScreenNode = CRAYON.PostProcessNode.extend( {
 		});
 
 		
-		this._super( renderer, shaderMaterial );
+		CRAYON.PostProcessNode.call( this, renderer, shaderMaterial );
 		this.inputs.requires('texture');
 
 	},
